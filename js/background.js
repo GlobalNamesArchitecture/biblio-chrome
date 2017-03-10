60cb9807aa175907740cea8733bc7a68a9b42cd4
David Shorthouse <davidpshorthouse@gmail.com>
2017-03-10 14:39:45 -0500

    Update pattern in background.js

--- js/background.js
+++ js/background.js
@@ -1,102 +1,110 @@
+/*global jQuery, window, document, self, chrome, console, _gaq */
 chrome.config = (function() {
+
+  "use strict";
+
   var configObject = false, xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) { configObject = JSON.parse(xhr.responseText); }
+    if (xhr.readyState === 4) { configObject = JSON.parse(xhr.responseText); }
   };
-  xhr.open("GET", chrome.extension.getURL('/config.json'), false);
+  xhr.open("GET", chrome.extension.getURL("/config.json"), false);
 
   try {
     xhr.send();
   } catch(e) {
-    console.log('Couldn\'t load manifest.json');
+    console.log("Couldn't load manifest.json");
   }
 
   return configObject;
 })();
 
-$(function() {
+var BiblioChrome = (function($, window, document) {
 
-  var bs = { "citation" : "" };
+  "use strict";
 
-  bs.trackEvent = function(category, action) {
-    if (window._gaq !== undefined) { _gaq.push(['_trackEvent', category, action]); }
-  };
+  var _private = {
 
-  bs.createContexts = function() {
-    var self = this, parent;
+    citation: "",
 
-    chrome.contextMenus.create({
-      "title"    : chrome.i18n.getMessage("context_search"),
-      "contexts" : ["selection"],
-      "onclick"  : self.selectionClick
-    });
+    trackEvent: function(category, action) {
+      if (window._gaq !== undefined) { _gaq.push(["_trackEvent", category, action]); }
+    },
 
-  };
+    createContexts: function() {
+      chrome.contextMenus.create({
+        "title"    : chrome.i18n.getMessage("context_search"),
+        "contexts" : ["selection"],
+        "onclick"  : this.selectionClick.bind(this)
+      });
+    },
 
-  bs.verifyStructure = function(selection) {
-    var valid_pattern = new RegExp('^(?=.*[A-Z]){3,}(?=.*\d){4,}.+$');
-    if(selection.length <= chrome.config.min_citation_length) { return false; }
-    if(!selection.match(valid_pattern)) {
-      return false;
-    }
-    return true;
-  };
-
-  bs.selectionClick = function(info, tab) {
-    tab = null;
-    var self = bs;
-
-    self.trackEvent("search", "launch");
-
-    if(!self.verifyStructure(info.selectionText)) {
-      alert(chrome.i18n.getMessage("invalid"));
-      return false;
-    }
-
-    self.citation = info.selectionText;
-    self.makeRequest();
-  };
-
-  bs.makeRequest = function() {
-    var self = bs, doi = "";
-
-    $.ajax({
-      type     : 'POST',
-      url      : chrome.config.crossref_api,
-      contentType : "application/json; charset=utf-8",
-      dataType : 'json',
-      data     : JSON.stringify([ self.citation ]),
-      timeout  : 10000,
-      success  : function(data) {
-        if(data.hasOwnProperty('results') && data.results.length > 0) {
-          $.each(data.results, function() {
-            if (this.hasOwnProperty('doi') && this.hasOwnProperty('score') && this.score > 4.5) {
-              chrome.tabs.create({'url': this.doi});
-              self.trackEvent("search", "found");
-            } else {
-              alert(chrome.i18n.getMessage("not_found"));
-              self.trackEvent("search", "not found");
-            }
-          });
-        } else {
-          alert(chrome.i18n.getMessage("not_found"));
-        }
-      },
-      error    : function() {
-        self.citation = "";
-        alert(chrome.i18n.getMessage("request_timeout"));
-        self.trackEvent("search", "timeout");
+    verifyStructure: function(selection) {
+      var valid_pattern = new RegExp('^(?=.*[A-Z]){3,}(?=.*\d){4,}.+$');
+      if(selection.length <= chrome.config.min_citation_length) { return false; }
+      if(!selection.match(valid_pattern)) {
+        return false;
       }
-   });
+      return true;
+    },
+
+    selectionClick: function(info) {
+      this.trackEvent("search", "launch");
+      if(!this.verifyStructure(info.selectionText)) {
+        alert(chrome.i18n.getMessage("invalid"));
+        return false;
+      }
+      this.citation = info.selectionText;
+      this.makeRequest();
+    },
+
+    makeRequest: function() {
+      var self = this, doi = "";
+
+      $.ajax({
+        type     : "POST",
+        url      : chrome.config.crossref_api,
+        contentType : "application/json; charset=utf-8",
+        dataType : "json",
+        data     : JSON.stringify([ this.citation ]),
+        timeout  : 10000,
+        success  : function(data) {
+          if(data.hasOwnProperty("results") && data.results.length > 0) {
+            self.sendWebHook(data);
+            $.each(data.results, function() {
+              if (this.hasOwnProperty("doi") && this.hasOwnProperty("score") && this.score > 4.5) {
+                chrome.tabs.create({"url": this.doi});
+                self.trackEvent("search", "found");
+              } else {
+                alert(chrome.i18n.getMessage("not_found"));
+                self.trackEvent("search", "not found");
+              }
+            });
+          } else {
+            alert(chrome.i18n.getMessage("not_found"));
+          }
+        },
+        error    : function() {
+          self.citation = "";
+          alert(chrome.i18n.getMessage("request_timeout"));
+          self.trackEvent("search", "timeout");
+        }
+      });
+    },
+
+    sendWebHook: function(message) {
+      chrome.tabs.query({active : true, currentWindow : true}, function(tab) {
+        tab = tab[0];
+        alert(tab.id);
+      });
+    },
+
+    init: function() {
+      this.createContexts();
+    }
 
   };
 
-  bs.init = function() {
-    this.createContexts();
-  };
+  _private.init();
 
-  bs.init();
-
-});
-
+}(jQuery, window, document));
